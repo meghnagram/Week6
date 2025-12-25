76929f9b83866f538749961257490ef8a1d08919
meghnagram <meghna.mona@gmail.com>
2025-12-25 18:26:33 +0530

    Create Number1.py
    Practice Well

---
+++ Number1.py
@@ -0,0 +1,20 @@
+def percentage_increased(original, new):
+    '''Calculate the percentage increase from the original value to the new value.
+
+    Assume original is less than or equal to new.
+
+    Args:
+        original (float): The original value.
+        new (float): The new value.
+
+    Returns:
+        float: The percentage increase.
+
+    Examples:
+    >>> percentage_increased(50, 75)
+    50.0
+    >>> percentage_increased(80, 100)
+    25.0
+    '''
+    
+    return ((new - original)*100)/original
