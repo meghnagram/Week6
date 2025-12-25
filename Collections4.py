bfc1c665e4be774e95d9a2f20434ab3e8b960df0
meghnagram <meghna.mona@gmail.com>
2025-12-25 19:43:59 +0530

    Create Collections4.py

---
+++ Collections4.py
@@ -0,0 +1,20 @@
+def unique_vowels(s: str) -> set:
+    '''
+    Given a string, return a set of unique vowels present in the string.
+
+    Arguments:
+    s: str - the input string
+
+    Return:
+    set - a set of unique vowels present in the string
+
+    Examples:
+    >>> unique_vowels('banana treat')
+    {'a', 'e'}
+    >>> unique_vowels('apple lolipop')
+    {'a', 'e', 'i', 'o'}
+    >>> unique_vowels('Ian Avinkov')
+    {'I','A','a','i','o'}
+    '''
+    
+    return set(s) & set("aeiou"+"aeiou".upper())
